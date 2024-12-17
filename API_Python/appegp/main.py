0633731ed807b9c410a880e87ecec2901a375149
rui-nobre-alpt <rui.nobre@altice.pt>
2024-12-17 15:48:40 +0000

    .

--- API_Python/appegp/main.py
+++ API_Python/appegp/main.py
@@ -14,6 +14,7 @@ def read_root():
 @app.post("/upload-egp/")
 async def upload_egp(file: UploadFile = File(...)):
     file_location = f"files_egp/{file.filename}"
+    os.makedirs(os.path.dirname(file_location), exist_ok=True)
     with open(file_location, "wb+") as file_object:
         file_object.write(file.file.read())
 
--- API_Python/appegp/src/analyzer.py
+++ API_Python/appegp/src/analyzer.py
@@ -2,15 +2,27 @@ import zipfile
 import xml.etree.ElementTree as ET
 
 
+import zipfile
+import re
+
+
 def analyze_egp_file(file_path):
     tables = []
+    table_pattern = re.compile(
+        r'\bFROM\s+(dcisas_k|dme_prd)\.\w+', re.IGNORECASE)
+
     with zipfile.ZipFile(file_path, 'r') as zip_ref:
         for file in zip_ref.namelist():
-            if file.endswith('.xml'):
-                with zip_ref.open(file) as xml_file:
-                    tree = ET.parse(xml_file)
-                    root = tree.getroot()
-                    for elem in root.iter():
-                        if elem.tag == 'Table' and ('dcisas_k' in elem.text or 'dme_prd' in elem.text):
-                            tables.append(elem.text)
-    return tables
+            if file.endswith('.log'):
+                with zip_ref.open(file) as log_file:
+                    for line in log_file:
+                        print(line)
+                        line = line.decode('utf-8')
+                        # matches = table_pattern.findall(line)
+                        if "FROM" in line:
+                            tokens = line.split(" ")
+                            print(tokens)
+                        # print(matches)
+                        # for match in matches:
+                        #    tables.append(match)
+    return list(set(tables))  # Remove duplicates
